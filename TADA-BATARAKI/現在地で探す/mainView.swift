814dfd2bd9a461d2e971fca0603ed444dd5b143e
笹倉一也 <>
2019-01-25 19:46:20 +0900

    add sections ....

--- TADA-BATARAKI/現在地で探す/mainView.swift
+++ TADA-BATARAKI/現在地で探す/mainView.swift
@@ -7,15 +7,15 @@
 //
 
 import UIKit
-import MapKit
-import CoreLocation
+import RealmSwift
 
 class mainView: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
