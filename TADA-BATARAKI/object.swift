f7431762f25e5a823ffff534dd49d46a456cc17f
笹倉一也 <>
2019-01-26 17:40:52 +0900

    next todouhukenn ....

---
+++ TADA-BATARAKI/object.swift
@@ -0,0 +1,21 @@
+//
+//  object.swift
+//  TADA-BATARAKI
+//
+//  Created by 笹倉一也 on 2019/01/26.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+class TadaBataraki: Object {
+    
+    @objc dynamic var name: String = ""
+    @objc dynamic var dokode: String = ""
+    
+    
+    
+    
+}
